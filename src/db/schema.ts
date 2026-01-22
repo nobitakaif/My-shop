f1c6855592b6cc571c7086426c8d15a58d5de447
mohd kaif <mk2818356@gmail.com>
2026-01-23 03:07:11 +0530

    added schema

---
+++ src/db/schema.ts
@@ -0,0 +1,40 @@
+
+import { varchar,numeric, pgEnum, pgTable, integer, text, uuid, timestamp} from "drizzle-orm/pg-core";
+
+
+const badgeValues = ["New", "Sale", "Featured", "Limited"] as const
+const inventoryValues = ["in-stock", "backorder", "preorder"]as const
+
+export const badgeEnum = pgEnum('badge', badgeValues)
+export const invertoryEnum = pgEnum("inventory", inventoryValues)
+
+export const products = pgTable("products", {
+    id : uuid('id').primaryKey().defaultRandom(),
+    name : varchar('name', {length : 256}).notNull(),
+    description : text('description').notNull(),
+    PrinterCheck : numeric('price', {precision : 10, scale : 2}).notNull(),
+    badge : badgeEnum('badge'),
+    rating : numeric('rating', {precision : 3, scale : 2}).notNull().default('0'),
+    reviews : integer('reviews').notNull().default(0),
+    image : varchar('image', {length : 512}).notNull(),
+    inventory : invertoryEnum('inventory').notNull().default('in-stock'),
+    createdAt : timestamp('created_at').defaultNow().notNull()
+})
+export type ProductSelect = typeof products.$inferSelect
+export type ProductInsert = typeof products.$inferSelect
+
+export const cartItems = pgTable('cart_items',{
+    id : uuid('id').primaryKey().defaultRandom(),
+    productId : uuid('product_id').notNull().references(()=> products.id, {onDelete : 'cascade'}),
+    quantity : integer('quantity').notNull().default(1),
+    createdAt : timestamp('created_at').defaultNow().notNull(),
+    updatedAt : timestamp('updated_at').defaultNow().notNull()
+})
+
+export type CartItemSelect = typeof cartItems.$inferSelect
+export type CartItemInsert = typeof cartItems.$inferInsert
+
+// export enum
+
+export type BadgeValue = (typeof badgeValues)[number]
+export type InvertoryEnum = (typeof inventoryValues)[number]
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -49,4 +49,95 @@ export const sampleProducts =[
         image : "",
         inventory : "in-stock"
     },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "in-stock"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "backorder"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "preorder"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "in-stock"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "in-stock"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "in-stock"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "backorder"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "preorder"
+    },
+    {
+        name : "TanStack Router Pro",
+        description : "The most powerful routiing solution for React. Build with TypeScript featuring type-safe routes, code splitting, and server-side rendering",
+        price : "99.99",
+        badge : "New",
+        rating : "4.8",
+        reviews : 127,
+        image : "",
+        inventory : "in-stock"
+    },
+    
 ]
